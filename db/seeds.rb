2308ab133c1456838c0155b2b86e40d21d8ccf4f
Jeff Taggart <jeff@jetaggart.com>
2014-08-27 09:15:31 -0600

    Fix styling on flash

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@ User.destroy_all
 TaskList.destroy_all
 
 User.create!(
-  name: "Some User",
-  email: 'user@example.com',
-  password: 'password',
-  password_confirmation: 'password'
+  :name => "Some User",
+  :email => 'user@example.com',
+  :password => 'password',
+  :password_confirmation => 'password'
 )
 
-TaskList.create!(name: "Work List")
-TaskList.create!(name: "Household Chores")
+TaskList.create!(:name => "Work List")
+TaskList.create!(:name => "Household Chores")
